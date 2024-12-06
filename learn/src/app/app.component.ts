bb8d1a6e837242b71f477892f6354381a0bd5614
sojwal-04 <sojwalgosavi1999@gmail.com>
2024-12-06 17:13:22 +0530

    05 Dec 2024
    -learnt about ng if  dirctive (conditionally rendering the element using NgIf directive)

--- learn/src/app/app.component.ts
+++ learn/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserComponent } from './components/user/user.component';
 import { AdminComponent } from './components/admin/admin.component';
+import { NgIfComponent } from './components/ng-if/ng-if.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UserComponent, AdminComponent],
+  imports: [RouterOutlet, UserComponent, AdminComponent, NgIfComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- learn/src/app/components/ng-if/ng-if.component.ts
+++ learn/src/app/components/ng-if/ng-if.component.ts
@@ -1,12 +1,23 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-ng-if',
   standalone: true,
-  imports: [],
+  imports: [NgIf, FormsModule],
   templateUrl: './ng-if.component.html',
-  styleUrl: './ng-if.component.css'
+  styleUrl: './ng-if.component.css',
 })
 export class NgIfComponent {
+  showDiv: boolean = false;
+
+  handleDivVisibility(toShow: boolean): void {
+    this.showDiv = toShow;
+  }
+
+  firstNum: string = '';
+  secondNum: string = '';
+
 
 }
