b2ccf8d4b97d2fed3d051385fe3a7ef01feaa9fa
sojwal-04 <sojwalgosavi1999@gmail.com>
2024-12-08 16:02:08 +0530

    06 Dec 2024
    - ng For directive

--- learn/src/app/app.component.ts
+++ learn/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { UserComponent } from './components/user/user.component';
 import { AdminComponent } from './components/admin/admin.component';
 import { NgIfComponent } from './components/ng-if/ng-if.component';
+import { NgForComponent } from './components/ng-for/ng-for.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UserComponent, AdminComponent, NgIfComponent],
+  imports: [RouterOutlet, UserComponent, AdminComponent, NgIfComponent, NgForComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- learn/src/app/components/ng-for/ng-for.component.ts
+++ learn/src/app/components/ng-for/ng-for.component.ts
@@ -1,12 +1,49 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
+type Employee = {
+  id: number | null,
+  name: string;
+  email: string;
+  mobile: string;
+};
 @Component({
   selector: 'app-ng-for',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './ng-for.component.html',
-  styleUrl: './ng-for.component.css'
+  styleUrl: './ng-for.component.css',
 })
 export class NgForComponent {
+  cityList: Array<String> = [
+    'New York',
+    'Mumbai',
+    'London',
+    'Jaipur',
+    'Mithril',
+    'San Francisco',
+    'Nashik',
+    'Tarapur',
+  ];
 
+  employees: Employee[] = [
+    {
+      id: 1,
+      name: 'Sojwal Gosavi',
+      email: 'sojwal@gmail.com',
+      mobile: '8421882288',
+    },
+    {
+      id: 2,
+      name: 'Jane Doe',
+      email: 'jane@example.com',
+      mobile: '8421882288',
+    },
+    {
+      id: 3,
+      name: 'James Doe',
+      email: 'jane@example.com',
+      mobile: '8421882288',
+    },
+  ];
 }
